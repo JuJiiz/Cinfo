61bca7364f70b0ab1c156d0055e208a73e192790
Dover <agitoattawun3@gmail.com>
2017-07-14 17:29:26 +0700

    17:27 14/7/2560 เพิ่ม modelHousehold แก้ไข modelToken

--- app/src/main/java/th/co/cinfo/chumchon/models/ModelToken.java
+++ app/src/main/java/th/co/cinfo/chumchon/models/ModelToken.java
@@ -1,24 +1,26 @@
 package th.co.cinfo.chumchon.models;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
-
-/**
- * Created by ZaiMon on 7/7/2560.
- */
 
 public class ModelToken {
 
     public static String getByName(String ptoken,String pname){
         String result = "";
         try {
-            String url = "https://api.cinfo.co.th/v1/userData?token="+ptoken;
+            String url = "https://api.cinfo.co.th/v2/userData?token=" + ptoken;
             String strGetJson = new CallApi().execute(url).get();
 
+            String[] keyNames = pname.split("\\.");
             JSONObject jsonObject = new JSONObject(strGetJson);
-            result = jsonObject.getString(pname);
+            for (int i = 0; i < keyNames.length; i++){
+                if(i == keyNames.length-1){
+                    result = jsonObject.getString(keyNames[i]);
+                }else{
+                    jsonObject = jsonObject.getJSONObject(keyNames[i]);
+                }
+            }
 
         } catch (InterruptedException e) {
             e.printStackTrace();
